9ac54b5c3041af2972b2292c6becc866fb42beff
Christian Dullweber <xchrdw@gmail.com>
2015-05-04 13:56:01 +0200

    use scala.html files in subproject

--- prolod-server/app/controllers/prolod/server/Application.scala
+++ prolod-server/app/controllers/prolod/server/Application.scala
@@ -2,10 +2,14 @@ package controllers.prolod.server
 
 import play.api.mvc.{Action, Controller}
 
+
 object Application extends Controller {
 
-   def index = Action {
-     Ok("test"/*views.html.Index()*/)
-   }
+  def index = Action {
+    Ok(views.html.Server())
+  }
+  def index2 = Action {
+    Ok(views.html.prolod.server.Server2())
+  }
 
  }
