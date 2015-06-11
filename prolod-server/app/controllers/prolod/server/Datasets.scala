f8bb184061f21a66c722fd26c84f1a183eb176b6
Anja Jentzsch <anja@anjeve.de>
2015-06-11 11:33:54 +0200

    connection to db for loading datasets and import

--- prolod-server/app/controllers/prolod/server/Datasets.scala
+++ prolod-server/app/controllers/prolod/server/Datasets.scala
@@ -9,7 +9,6 @@ import play.api.libs.json._
 
 object Datasets extends Controller {
 
-<<<<<<< HEAD
 	def datasets = Action {
 		var config = new Configuration()
 		var db = new DatabaseConnection(config)
@@ -17,31 +16,4 @@ object Datasets extends Controller {
 		val json = Json.obj("datasets" -> data)
 		Ok(json)
 	}
-=======
-  val list = List(
-    Dataset(
-      0,
-      "DBpedia",
-      2340000,
-      List(
-        Group(0, "dbpedia:Person", 700000),
-        Group(1, "dbpedia:Place", 30000))
-    ),
-    Dataset(
-      1,
-      "DrugBank",
-      5,
-      List(
-        Group(0, "Drugs", 3),
-        Group(1, "Diseases", 2)
-      )
-    )
-  )
-
-  def datasets = Action {
-    val data: List[Dataset] = list
-    val json = Json.obj("datasets" -> data)
-    Ok(json)
-  }
->>>>>>> 9270dfea5e135935579d70e24d13e8ca1edbdec2
 }
