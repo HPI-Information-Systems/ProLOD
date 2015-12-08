98afe5778b291860fbea839025bde03bfbd5ecce
Christian Dullweber <xchrdw@gmail.com>
2015-12-08 18:40:10 +0100

    remove circles and multiple inheritance from class graph

--- prolod-server/app/controllers/prolod/server/Classes.scala
+++ prolod-server/app/controllers/prolod/server/Classes.scala
@@ -6,20 +6,28 @@ import prolod.common.config.{Configuration, DatabaseConnection}
 import prolod.common.models.ClassHierarchy
 import prolod.common.models.ClassHierarchyFormats.classHierarchyFormat
 
+import scala.collection.mutable
+
 object Classes extends Controller {
 
 	def classes(dataset: String) = Action {
 		val config = new Configuration()
 		val db = new DatabaseConnection(config)
 		val data = db.getClassHierarchy(dataset)
-		val hierarchy = buildHierarchy("http://www.w3.org/2002/07/owl#Thing", data)
+		val seenClasses = new mutable.HashSet[String]()
+		val hierarchy = buildHierarchy("http://www.w3.org/2002/07/owl#Thing", data, seenClasses)
 		val json = Json.obj("classes" -> hierarchy)
 		Ok(json)
 	}
 
-	def buildHierarchy(className: String, data: Map[String, Seq[String]]): ClassHierarchy = {
-		val children = data.getOrElse(className, Seq()).map(child => buildHierarchy(child, data))
-		val size = children.map(c => c.size).sum + 1
-		new ClassHierarchy(className, size, children)
+	def buildHierarchy(className: String, data: Map[String, Seq[String]], seenClasses: mutable.Set[String]): ClassHierarchy = {
+		if(seenClasses.contains(className)) {
+			return null // return null when a circle or multiple inheritance is found 
+		}
+		seenClasses.add(className)
+		val children = data.getOrElse(className, Seq()).map(child => buildHierarchy(child, data, seenClasses)).filter(p => p!=null)
+		val ownSize = 1 // TODO calculate size
+		val totalSize = children.map(c => c.size).sum + ownSize
+		new ClassHierarchy(className, totalSize, children)
 	}
 }
