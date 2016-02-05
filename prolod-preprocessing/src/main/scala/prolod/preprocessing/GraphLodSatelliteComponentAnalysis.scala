6ef05c698e99547f03e92d18cd33655c747fbb09
Christian Dullweber <xchrdw@gmail.com>
2016-02-05 17:33:08 +0100

    fix satelite components

--- prolod-preprocessing/src/main/scala/prolod/preprocessing/GraphLodSatelliteComponentAnalysis.scala
+++ prolod-preprocessing/src/main/scala/prolod/preprocessing/GraphLodSatelliteComponentAnalysis.scala
@@ -1,14 +1,16 @@
 package prolod.preprocessing
 
+import com.typesafe.scalalogging.LazyLogging
+import graphlod.GraphLOD
 import graphlod.dataset.Dataset
 import prolod.common.config.DatabaseConnection
 
-class GraphLodSatelliteComponentAnalysis(var db: DatabaseConnection, var name : String, var dataset: Dataset) {
+class GraphLodSatelliteComponentAnalysis(var db: DatabaseConnection, var name : String, var dataset: Dataset) extends LazyLogging {
 	//val excludedNamespaces : List[String] = Nil
-	val graphLodPatternSimilarity : graphlod.SatelliteComponentAnalysis = new graphlod.SatelliteComponentAnalysis(name, dataset)
+	val sateliteComponents : graphlod.SatelliteComponentAnalysis = new graphlod.SatelliteComponentAnalysis(name, dataset)
 
 	def run: Unit = {
-		val graphLodInstance: GraphLOD = graphLod.getGraphLodInstance
+		val graphLodInstance: GraphLOD = sateliteComponents.getGraphLodInstance
 		logger.info("Insert patterns into DB")
 		db.insertPatterns(name, graphLodInstance.patterns, graphLodInstance.coloredPatterns, graphLodInstance.colorIsomorphicPatterns, graphLodInstance.patternDiameter, None)
 	}
